b7333152e716066e9f33f091dabbbb72c6391dbe
ThomasFregene <fregzythomas@gmail.com>
2022-07-26 15:57:53 +0100

    working with mock repo

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    //[Route("[controller]")]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommand();
+            return Ok(commandItems);
+        }
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+
+}
