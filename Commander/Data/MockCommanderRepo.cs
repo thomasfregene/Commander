b5b713f6e3eb03aa644e46c7e1ffdfc46c021f0e
ThomasFregene <fregzythomas@gmail.com>
2022-07-26 15:28:14 +0100

    initial commit

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,34 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommand()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=1, HowTo = "initialize git", Line="git init", Platform="git"},
+                new Command{Id=2, HowTo = "clone repo", Line="git clone", Platform="github"},
+                new Command{Id=1, HowTo = "create nodule module", Line="npm init", Platform="nodejs"}
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command
+            {
+                Id = 1,
+                HowTo = "get command",
+                Line = "run command",
+                Platform = "git"
+            };
+        }
+    }
+}
