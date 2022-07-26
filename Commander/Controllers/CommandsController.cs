a103c0f8a3b0a7a285f4d32fd9a15975a09e5347
ThomasFregene <fregzythomas@gmail.com>
2022-07-26 16:14:14 +0100

    using dependency injection

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -13,7 +13,13 @@ namespace Commander.Controllers
     //[Route("[controller]")]
     public class CommandsController : ControllerBase
     {
-        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        private readonly ICommanderRepo _repository;
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+        //private readonly MockCommanderRepo _repository = new MockCommanderRepo();
         //GET api/commands
         [HttpGet]
         public ActionResult<IEnumerable<Command>> GetAllCommands()
